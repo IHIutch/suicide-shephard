0021663fb0876d0d73c85946059f3c724b5e77c6
IHIutch <jbhutch01@gmail.com>
2020-06-03 09:37:48 -0400

    Start of page layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import "~/assets/scss/main.scss";
+import { BootstrapVue } from "bootstrap-vue";
 
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
+  Vue.use(BootstrapVue);
+
   Vue.component("Layout", DefaultLayout);
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,21 +1,76 @@
 <template>
   <Layout>
-    Hey
+    <b-container fluid>
+      <b-row>
+        <b-col cols="3"></b-col>
+        <b-col cols="9">
+          <div v-for="(sound, idx) in sounds" class="border rounded mb-4 p-4">
+            <div class="d-flex justify-content-between">
+              <div>
+                <a :href="sound.link">{{ sound.artist }} - {{ sound.title }}</a>
+              </div>
+              <div>
+                <span>
+                  {{ sound.datePosted | dayjs("MMMM D, YYYY - h:mmA") }}
+                </span>
+              </div>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </Layout>
 </template>
 
+<page-query>
+query {
+  sounds: allSound {
+    edges {
+      node {
+        id
+        title
+        coverArt
+        artist
+        datePosted
+        tag
+        link
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import dayjs from "dayjs";
+
 export default {
   metaInfo: {
     title: "Hello, world!",
   },
   data() {
     return {
-      tracks: [],
+      sounds: [],
     };
   },
   methods: {},
-  created() {},
+  created() {
+    this.sounds = this.$page.sounds.edges.map((sound) => {
+      let node = sound.node;
+      return {
+        id: node.id,
+        title: node.title,
+        coverArt: node.coverArt,
+        artist: node.artist,
+        datePosted: node.datePosted,
+        tag: node.tag,
+        link: `https://soundcloud.com${node.link}`,
+      };
+    });
+  },
+  filters: {
+    dayjs(val, format) {
+      return dayjs(val).format(format);
+    },
+  },
 };
 </script>
-npm audit
