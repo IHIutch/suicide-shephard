0ad6fce827ae247db077ab80861d25c9d9b89de9
IHIutch <jbhutch01@gmail.com>
2020-06-03 20:13:05 -0400

    Starting filters

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,78 @@
   <Layout>
     <b-container fluid>
       <b-row>
-        <b-col cols="3"></b-col>
-        <b-col cols="9">
+        <b-col
+          md="3"
+          lg="2"
+          class="position-fixed top-0 left-0 bottom-0 px-0 bg-light"
+        >
+          <div class="p-3 h-100 overflow-auto">
+            <h2>Filters</h2>
+            <div class="mb-4">
+              <div>
+                <span
+                  class="text-sm font-weight-semibold text-uppercase tracking-wider"
+                >
+                  Published Date
+                </span>
+              </div>
+              <div>
+                <b-form-datepicker
+                  id="publishStart"
+                  v-model="filter.publishStart"
+                  placeholder="Start date"
+                  class="mb-2"
+                  hide-header
+                ></b-form-datepicker>
+                <b-form-datepicker
+                  id="publishEnd"
+                  v-model="filter.publishEnd"
+                  placeholder="End date"
+                  hide-header
+                ></b-form-datepicker>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div>
+                <span
+                  class="text-sm font-weight-semibold text-uppercase tracking-wider"
+                >
+                  Artist
+                </span>
+              </div>
+              <div>
+                <b-form-group label="Artists">
+                  <b-form-checkbox-group
+                    v-model="filter.artist"
+                    :options="artistList"
+                    name="artist"
+                    stacked
+                  ></b-form-checkbox-group>
+                </b-form-group>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div>
+                <span
+                  class="text-sm font-weight-semibold text-uppercase tracking-wider"
+                >
+                  Tag
+                </span>
+              </div>
+              <div>
+                <b-form-group label="Tags">
+                  <b-form-checkbox-group
+                    v-model="filter.tag"
+                    :options="tagList"
+                    name="tag"
+                    stacked
+                  ></b-form-checkbox-group>
+                </b-form-group>
+              </div>
+            </div>
+          </div>
+        </b-col>
+        <b-col md="9" lg="10" class="ml-auto">
           <div v-for="(sound, idx) in sounds" class="border rounded mb-4 p-4">
             <div class="d-flex justify-content-between">
               <div>
@@ -50,6 +120,12 @@ export default {
   data() {
     return {
       sounds: [],
+      filter: {
+        publishStart: "",
+        publishEnd: "",
+        artist: [],
+        tag: [],
+      },
     };
   },
   methods: {},
@@ -66,6 +142,14 @@ export default {
         link: `https://soundcloud.com${node.link}`,
       };
     });
+  },
+  computed: {
+    artistList() {
+      return [...new Set(this.sounds.map((sound) => sound.artist))].sort();
+    },
+    tagList() {
+      return [...new Set(this.sounds.map((sound) => sound.tag))].sort();
+    },
   },
   filters: {
     dayjs(val, format) {
